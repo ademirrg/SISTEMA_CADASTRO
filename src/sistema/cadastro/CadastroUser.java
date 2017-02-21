9d0717ae1dec421ee9625c017d58e9dcee3810a0
ademirrg <ademir_rg@yahoo.com.br>
2017-02-21 20:20:36 -0300

    Correcão de bugs

--- src/sistema/cadastro/CadastroUser.java
+++ src/sistema/cadastro/CadastroUser.java
@@ -3,13 +3,11 @@ import java.awt.Color;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.List;
-
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JPasswordField;
-import javax.swing.JScrollPane;
 import javax.swing.JTable;
 import javax.swing.JTextField;
 
