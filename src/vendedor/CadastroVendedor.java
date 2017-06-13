2361dec8812602f567a66b6de3082f7107ba36da
rogarcia <rogarcia@LP1045>
2017-06-13 17:26:18 -0300

    ...

--- src/vendedor/CadastroVendedor.java
+++ src/vendedor/CadastroVendedor.java
@@ -21,7 +21,8 @@ public class CadastroVendedor extends JFrame implements ActionListener{
 	//Tela
 	setTitle("CADASTRAMENTO DE VENDEDOR");
 	setSize(700, 500);
-	setLocation(450, 100);
+	//setLocation(450, 100);
+	setLocationRelativeTo(null);
 	setResizable(false);
 	setLayout(null);
 	getContentPane().setBackground(Color.LIGHT_GRAY);
